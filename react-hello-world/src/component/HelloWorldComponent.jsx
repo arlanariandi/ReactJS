b5b4c2107bf3043a90e6e6422c2d131415ce424a
Arlan Ariandi Trislis <arlanariandi@gmail.com>
2024-04-21 12:25:12 +0700

    UPDATE: StateLess and StateFull Component

---
+++ react-hello-world/src/component/HelloWorldComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './HelloWorldComponent.css';
+
+// Stateless / functional component
+const HelloWorldComponent = () => {
+    return <p className={'text-p'}>Hello World Component</p>
+}
+
+export default HelloWorldComponent;
---
+++ react-hello-world/src/container/StateFullComponent.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+// StateFull / class component
+class StateFullComponent extends Component {
+    render() {
+        return <p>StateFullComponent</p>
+    }
+}
+
+export default StateFullComponent;
